8f685d399e5fdca1a45dd59f8b92311bf1b2b4c9
unknown <thijsvanweezel@gmail.com>
2023-10-01 13:03:48 +0200

    separate utils

---
+++ utils.py
@@ -0,0 +1,30 @@
+import tensorflow as tf
+from models import BATCH_SIZE
+
+# Define function for calculating and applying perturbations
+def fgsm(classifier, clf_loss_fn, imgs, labels, epsilon=tf.constant(0.01)):
+    with tf.GradientTape() as tape:
+        tape.watch(imgs)
+        predictions = classifier(imgs)
+        loss = clf_loss_fn(labels, predictions)
+
+    # Get the gradients of the loss w.r.t to the input image.
+    gradients = tape.gradient(loss, imgs)
+    # Get the sign of the gradients to create the perturbation
+    signed_gradients = tf.sign(gradients)
+    return imgs + epsilon*signed_gradients
+
+# Define dataset transformations
+def dataset_from_arrays(x, y):
+    return tf.data.Dataset.from_tensor_slices(
+        (x, y)
+        ).map(
+            # Transform for compatibility with 2d conv
+            lambda x, y: (tf.expand_dims(x, axis=-1), y)
+        ).map(
+            # Resize for decoding stability
+            lambda x, y: (tf.image.resize(x, (32,32)), y)
+        ).map(
+            # One-hot encode labels
+            lambda x, y: (x, tf.one_hot(y, depth=10))
+        ).batch(BATCH_SIZE).cache().repeat()
