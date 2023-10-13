056bd4a384e3587b0b63822b5809bb336e1d599f
unknown <thijsvanweezel@gmail.com>
2023-10-13 19:08:36 +0200

    separate evaluation functionality

---
+++ evaluate.py
@@ -0,0 +1,29 @@
+import tensorflow as tf
+
+def evaluate(dataset, max_i, encoder, decoder, classifier, adv_attack):
+    # Initialize scores
+    correct_original = 0
+    correct_reconstructed = 0
+    correct_adv = 0
+    # Loop over batches and predict
+    for i, (imgs, labels) in enumerate(dataset):
+        if i==dataset.cardinality or i==max_i:
+            break
+        # For originals
+        original_predictions = classifier(imgs)
+        correct_original += tf.reduce_sum(tf.cast(tf.argmax(original_predictions, axis=1)==tf.argmax(labels, axis=1), dtype=tf.int32))
+        # For adversarials
+        adv_imgs = adv_attack(classifier, tf.keras.losses.CategoricalCrossentropy(), imgs, labels)
+        adv_predictions = classifier(adv_imgs)
+        correct_adv += tf.reduce_sum(tf.cast(tf.argmax(adv_predictions, axis=1)==tf.argmax(labels, axis=1), dtype=tf.int32))
+        # For reconstructions
+        z = encoder(adv_imgs)
+        reconstructed = decoder(z)
+        predictions = classifier(reconstructed)
+        correct_reconstructed += tf.reduce_sum(tf.cast(tf.argmax(predictions, axis=1)==tf.argmax(labels, axis=1), dtype=tf.int32))
+
+        return {
+            "acc_original":correct_original/i,
+            "acc_adv":correct_adv/i,
+            "acc_reconstructed":correct_reconstructed/i
+            }
