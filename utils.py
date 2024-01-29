9989706c302888a35e473c35ee864a867f243e43
unknown <thijsvanweezel@gmail.com>
2024-01-29 16:15:01 +0900

    make evaluation more flexible

--- utils.py
+++ utils.py
@@ -80,43 +80,6 @@ class SSIM_L1(tf.keras.losses.Loss):
         l1 = tf.reduce_mean(self.l1(y_true, y_pred), axis=tf.range(1, tf.rank(y_true)-1), keepdims=True)
         return self.alpha*ssim + (1-self.alpha)*l1
 
-# def evaluate(dataset, max_i, classifier, ae, adv_attack, epsilon):
-#     """
-#     Calculates the accuracy of the classifier before and after purification.\n
-#     Args:\n
-#     \t- dataset: The `tf.data.Dataset` to evaluate on.\n
-#     \t- max_i: In case of an infinitely repeating dataset, the maximum samples.\n
-#     \t- classifier: The classifier to evaluate.\n
-#     \t- ae: The purification model.\n
-#     \t- adv_attack: The adversarial attack function.\n
-#     \t- epsilon: The epsilon value for the adversarial attack.
-#     """
-#     # Initialize scores 
-#     correct_original = 0
-#     correct_reconstructed = 0
-#     correct_adv = 0
-#     # Loop over batches and predict
-#     for i, (imgs, labels) in enumerate(dataset):
-#         if i==dataset.cardinality or i==max_i:
-#             break
-#         # For originals
-#         original_predictions = classifier(imgs, training=False)
-#         correct_original += tf.reduce_sum(tf.cast(tf.argmax(original_predictions, axis=1)==tf.argmax(labels, axis=1), dtype=tf.int32))
-#         # For adversarials
-#         adv_imgs = adv_attack(classifier, imgs, epsilon)
-#         adv_predictions = classifier(adv_imgs, training=False)
-#         correct_adv += tf.reduce_sum(tf.cast(tf.argmax(adv_predictions, axis=1)==tf.argmax(labels, axis=1), dtype=tf.int32))
-#         # For reconstructions
-#         reconstructed = ae(adv_imgs, training=False)
-#         predictions = classifier(reconstructed, training=False)
-#         correct_reconstructed += tf.reduce_sum(tf.cast(tf.argmax(predictions, axis=1)==tf.argmax(labels, axis=1), dtype=tf.int32))
-
-#     return {
-#         "acc_original":float(correct_original/(i*BATCH_SIZE)),
-#         "acc_adv":float(correct_adv/(i*BATCH_SIZE)),
-#         "acc_reconstructed":float(correct_reconstructed/(i*BATCH_SIZE))
-#         }
-
 def evaluate(dataset, max_i, classifier, adv_attack=None, ae=None):
     """
     Calculates the accuracy of the classifier before and after purification.\n
