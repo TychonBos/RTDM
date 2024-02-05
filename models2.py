3b5c633c0ed2575748a1f6a14f24aae464394eca
unknown <thijsvanweezel@gmail.com>
2024-02-05 22:04:59 +0900

    convert to resnet

--- models2.py
+++ models2.py
@@ -1,7 +1,7 @@
 # Packages
 import tensorflow as tf
 # Constants
-FILTERS = [64,128,256,512]
+FILTERS = [32,64,64,128,128,256,256,512,512]
 LATENT_SHAPE = (None, None, FILTERS[-1])
 BATCH_SIZE = 64
 
@@ -16,7 +16,7 @@ def block(nfilters, size, strides, activation=None, name=None, convtype=None, dt
     """
     return tf.keras.Sequential([
         tf.keras.layers.Dropout(rate=.25, dtype=dtype),
-        (convtype or tf.keras.layers.Conv2D)(filters=nfilters, kernel_size=size, padding="same", strides=strides, dtype=dtype),
+        (convtype or tf.keras.layers.Conv2D)(filters=nfilters, kernel_size=size, padding="same", strides=strides, dtype=dtype, kernel_constraint=tf.keras.constraints.MaxNorm(3)),
         tf.keras.layers.GroupNormalization(groups=nfilters, dtype=dtype),
         activation or tf.keras.layers.LeakyReLU(dtype=dtype)
     ], name=name)
@@ -32,15 +32,30 @@ def build_encoder(channels, name="Encoder"):
     inputs = x = tf.keras.layers.Input(shape=(None, None, channels), batch_size=None, name="Input")
     # Create some downsampling blocks
     for i, nfilters in enumerate(FILTERS):
+        x_ = block(
+            convtype=tf.keras.layers.Conv2D,
+            nfilters=nfilters, 
+            size=(1,1), 
+            strides=(1,1), 
+            name=f"Skip_{i}"
+        )(x)
         x = block(
             convtype=tf.keras.layers.Conv2D,
             nfilters=nfilters, 
-            size=(12,12), 
+            size=(4,4), 
             strides=(1,1), 
-            name=f"Downsampler_{i}",
-            dtype=tf.float32 if i==(len(FILTERS)-1) else None
+            name=f"Downsampler_{i*2}",
         )(x)
-        x = tf.keras.layers.MaxPool2D(pool_size=(2,2))(x)
+        x = block(
+            convtype=tf.keras.layers.Conv2D,
+            nfilters=nfilters, 
+            size=(3,3), 
+            strides=(1,1), 
+            name=f"Downsampler_{i*2+1}",
+        )(x)
+        x = tf.keras.layers.Add(name=f"SumSkips_{i}")([x, x_])
+        if (i+1)%3==0:
+            x = tf.keras.layers.MaxPool2D()(x)
     return tf.keras.Model(inputs=inputs, outputs=x, name=name)
 
 # Define a function to build the decoder
@@ -54,14 +69,28 @@ def build_decoder(channels, name="Decoder"):
     inputs = x = tf.keras.layers.Input(shape=LATENT_SHAPE, batch_size=None, name="LatentInput")
     # Revert the downsampling
     for i, nfilters in enumerate(FILTERS):
+        x_ = block(
+            convtype=tf.keras.layers.Conv2DTranspose,
+            nfilters=nfilters, 
+            size=(1,1), 
+            strides=(2,2) if (i+1)%3==0 else (1,1), 
+            name=f"Skip_{i}"
+        )(x)
+        x = block(
+            convtype=tf.keras.layers.Conv2D,
+            nfilters=nfilters, 
+            size=(4,4), 
+            strides=(1,1), 
+            name=f"Upsampler_{i*2}",
+        )(x)
         x = block(
             convtype=tf.keras.layers.Conv2DTranspose,
             nfilters=nfilters, 
-            size=(12,12), 
-            strides=(2,2), 
-            name=f"Downsampler_{i}",
-            dtype=tf.float32 if i==(len(FILTERS)-1) else None
+            size=(3,3), 
+            strides=(2,2) if (i+1)%3==0 else (1,1), 
+            name=f"Upsampler_{i*2+1}",
         )(x)
+        x = tf.keras.layers.Add(name=f"SumSkips_{i}")([x, x_])
     # Reshape channels to image format
     outputs = block(
         nfilters=channels, 
@@ -84,20 +113,36 @@ def build_classifier(channels, nclasses, name="Classifier"):
     inputs = x = tf.keras.layers.Input(shape=(None, None, channels), batch_size=None, name="Input")
     # Create some downsampling blocks
     for i, nfilters in enumerate(FILTERS):
+        x_ = block(
+            convtype=tf.keras.layers.Conv2D,
+            nfilters=nfilters, 
+            size=(1,1), 
+            strides=(1,1), 
+            name=f"Skip_{i}"
+        )(x)
         x = block(
             convtype=tf.keras.layers.Conv2D,
             nfilters=nfilters, 
-            size=(12,12), 
+            size=(4,4), 
             strides=(1,1), 
-            name=f"Downsampler_{i}",
+            name=f"Downsampler_{i*2}",
         )(x)
-        x = tf.keras.layers.MaxPool2D(pool_size=(2,2))(x)
+        x = block(
+            convtype=tf.keras.layers.Conv2D,
+            nfilters=nfilters, 
+            size=(3,3), 
+            strides=(1,1), 
+            name=f"Downsampler_{i*2+1}",
+        )(x)
+        x = tf.keras.layers.Add(name=f"SumSkips_{i}")([x, x_])
+        if (i+1)%3==0:
+            x = tf.keras.layers.MaxPool2D()(x)
     x = tf.keras.layers.GlobalMaxPooling2D(name="Pooling")(x)
-    x = tf.keras.layers.Dense(units=512, activation=tf.keras.layers.LeakyReLU(), name="Dense1")(x)
+    x = tf.keras.layers.Dense(units=512, activation=tf.keras.layers.LeakyReLU(), name="Dense1", kernel_constraint=tf.keras.constraints.MaxNorm(3))(x)
     x = tf.keras.layers.Dropout(rate=.25)(x)
-    x = tf.keras.layers.Dense(units=256, activation=tf.keras.layers.LeakyReLU(), name="Dense2")(x)
+    x = tf.keras.layers.Dense(units=256, activation=tf.keras.layers.LeakyReLU(), name="Dense2", kernel_constraint=tf.keras.constraints.MaxNorm(3))(x)
     x = tf.keras.layers.Dropout(rate=.25)(x)
-    x = tf.keras.layers.Dense(units=128, activation=tf.keras.layers.LeakyReLU(), name="Dense3")(x)
+    x = tf.keras.layers.Dense(units=128, activation=tf.keras.layers.LeakyReLU(), name="Dense3", kernel_constraint=tf.keras.constraints.MaxNorm(3))(x)
     x = tf.keras.layers.Dropout(rate=.25)(x)
     outputs = tf.keras.layers.Dense(units=nclasses, dtype=tf.float32, activation=tf.keras.activations.softmax, name="Outputs")(x)
     return tf.keras.Model(inputs=inputs, outputs=outputs, name=name)
@@ -115,12 +160,14 @@ def build_models(channels, nclasses):
     # Combine because why not
     ae = tf.keras.Sequential([encoder, decoder], name="Autoencoder")
     # Share an optimizer
-    ae_optimizer = tf.keras.optimizers.Adam(epsilon=1e-3) 
+    scheduler = tf.keras.optimizers.schedules.ExponentialDecay(1e-3, decay_steps=5e3, decay_rate=.95)
+    ae_optimizer = tf.keras.optimizers.Adam(learning_rate=scheduler, epsilon=1e-4) 
     ae_optimizer = tf.keras.mixed_precision.LossScaleOptimizer(ae_optimizer)
 
     # Classifier
     classifier = build_classifier(channels, nclasses)
     # Optimizer
-    classifier_optimizer = tf.keras.optimizers.Adam(epsilon=1e-3, learning_rate=1e-5) 
+    scheduler = tf.keras.optimizers.schedules.ExponentialDecay(1e-3, decay_steps=5e3, decay_rate=.95)
+    classifier_optimizer = tf.keras.optimizers.Adam(learning_rate=scheduler, epsilon=1e-4) 
     classifier_optimizer = tf.keras.mixed_precision.LossScaleOptimizer(classifier_optimizer)
     return classifier, ae, ae_optimizer, classifier_optimizer
